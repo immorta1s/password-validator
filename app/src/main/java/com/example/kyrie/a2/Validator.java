75b59d6095bf6ee6175414c00e0268a1e3a26f23
Xiaoze Xia <crybaby3799@gmail.com>
2018-06-09 21:37:57 -0300

    2

--- app/src/main/java/com/example/kyrie/a2/Validator.java
+++ app/src/main/java/com/example/kyrie/a2/Validator.java
@@ -6,16 +6,33 @@ public class Validator {
     private int check = 0 ;
 
     public int validator(String input) {
-
+        //1st requirement
         if (!input.equals("passward")) {
             check++;
         }
 
+        //2nd requirement
         if (input.length() >= 8) {
             check++;
 
         }
 
+        //3rd requirement
+        if(!input.equals("Trump")){
+            check++;
+        }
+
+        //4th requirement
+        if(!(input.contains("@") | input.contains("!") | input.contains("#")){
+            check++;
+        }
+
+        //5th requirement
+        if(input.contains("1") | input.contains("2") | input.contains("3") |input.contains("4")
+                 |input.contains("5")|input.contains("6")|input.contains("7")|input.contains("8")
+                |input.contains("9")|input.contains("0")){
+            check++;
+        }
         return check;
     }
 
